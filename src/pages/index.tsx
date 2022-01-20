e7466f636c2314759a722c9160762b3bf19c7c0d
yeheum Choi <61037219+cyheum@users.noreply.github.com>
2022-01-20 21:28:04 +0900

    [ENH] : 사이드 네브 바 작업 (#4)
    * [ENH] : 메인 네브바 작업
    
    * [ENH] : 사이드 네브 바 작업

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,17 +7,14 @@ import { API } from '@/utils';
 import { Widgets } from '@/components';
 
 const HomePage: React.FC = () => {
-  const { test } = useGetStore.home();
+  const { selectedMenu } = useGetStore.home();
   const dispatch = useDispatch();
 
-  const onClickSetTest = (test: string) => {
-    dispatch(homeActions.setTest(test));
-  };
+  const onClickSetTest = (test: string) => {};
 
   const getData = async () => {
     try {
       const test = await API.GET('');
-      dispatch(homeActions.setTest(test));
     } catch (error) {
       console.error(error);
     }
@@ -25,8 +22,12 @@ const HomePage: React.FC = () => {
 
   return (
     <>
-      <div>hello {test}</div>
-      <Widgets.Button.Primary value="제품" state={'active'} />
+      <div>hello {selectedMenu}</div>
+      <Widgets.Button.Primary
+        value="제품"
+        state={'active'}
+        onClick={() => {}}
+      />
       <Widgets.Button.Secondary value="제품" state={'active'} />
       <Widgets.Button.Widget value="이미지" state={'default'} />
       <Home.Container onClickSetTest={onClickSetTest} />
