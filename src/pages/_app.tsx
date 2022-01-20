b140f60dc40b7836c70b9c0516b22f55a4728fbb
Yeheum Choi <yh.choi@purple-nest.com>
2022-01-20 10:24:01 +0900

    [ENH] : 메인 네브바 작업

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import styled from 'styled-components';
+import { useDispatch } from 'react-redux';
+import { MainNav } from '@/components';
 import { AppProps } from 'next/app';
 import {
   ApolloClient,
@@ -9,6 +12,14 @@ import {
 } from '@apollo/client';
 import withReduxSaga from 'next-redux-saga';
 import { wrapper } from '@/store/configureStore';
+import { useGetStore } from '@/hooks';
+import { mixins } from '@/styles';
+import { homeActions } from '@/store';
+
+const STDContainer = styled.div`
+  ${mixins.flexSet('flex-start')}
+  padding: 16px;
+`;
 
 const client = new ApolloClient({
   uri: 'https://48p1r2roz4.sse.codesandbox.io',
@@ -16,9 +27,19 @@ const client = new ApolloClient({
 });
 
 function App({ Component, pageProps }: AppProps) {
+  const dispatch = useDispatch();
+  const { selectedMenu } = useGetStore.home();
+
+  const onClickMenu = (menu: string) => {
+    dispatch(homeActions.setSelectedMenu(menu));
+  };
+
   return (
     <ApolloProvider client={client}>
-      <Component {...pageProps} />
+      <STDContainer>
+        <MainNav selectedMenu={selectedMenu} onClickMenu={onClickMenu} />
+        <Component {...pageProps} />
+      </STDContainer>
     </ApolloProvider>
   );
 }
