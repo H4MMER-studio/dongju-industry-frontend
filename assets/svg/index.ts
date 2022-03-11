d563240d585ed6915d3021af81358959a0fd7fbb
Yeheum Choi <yh.choi@purple-nest.com>
2022-03-11 13:34:07 +0900

    [ENH] : 레이아웃 구성

--- assets/svg/index.ts
+++ assets/svg/index.ts
@@ -1,5 +1,15 @@
 import IconHamburg from './icon_hamburg.svg';
 import IconDownArrow from './icon_down_arrow.svg';
 import IconClose from './icon_close.svg';
+import IconDownArrowSmall from './icon_down_arrow_small.svg';
+import IconDownArrowGray from './icon_down_arrow_gray.svg';
+import IconSearch from './icon_search.svg';
 
-export { IconHamburg, IconDownArrow, IconClose };
+export {
+  IconHamburg,
+  IconDownArrow,
+  IconClose,
+  IconDownArrowSmall,
+  IconSearch,
+  IconDownArrowGray,
+};
