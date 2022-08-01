ffaacd5e823d500aaa1713bca08f87e845908491
Gunoo-Kim <gunoo@cherground.com>
2022-08-01 21:48:22 +0900

    리뉴얼 안내

--- src/components/Layout/Customer-Service/CustomerServiceContainer.tsx
+++ src/components/Layout/Customer-Service/CustomerServiceContainer.tsx
@@ -1,126 +1,135 @@
-import React, { useState, useEffect } from 'react';
-import styled from 'styled-components';
-import * as CustomerServiceComponents from './Components';
-import { Images } from 'public/image';
-import useResize from '@/hooks/useResize';
-import { IForm } from '@/interfaces';
-import { useDispatch } from 'react-redux';
-import { customerServiceActions } from '@/store';
-import { useRouter } from 'next/router';
-import { useGetStore } from '@/hooks';
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+import * as CustomerServiceComponents from "./Components";
+import { Images } from "public/image";
+import useResize from "@/hooks/useResize";
+import { IForm } from "@/interfaces";
+import { useDispatch } from "react-redux";
+import { customerServiceActions } from "@/store";
+import { useRouter } from "next/router";
+import { useGetStore } from "@/hooks";
+import { Widgets } from "@/components";
 
 interface Iprops {
-  questionType: 'estimate' | 'A/S' | 'ETC';
-  clickContact: (type: 'estimate' | 'A/S' | 'ETC') => void;
-  closeForm: () => void;
+    questionType: "estimate" | "A/S" | "ETC";
+    clickContact: (type: "estimate" | "A/S" | "ETC") => void;
+    closeForm: () => void;
 }
 
+const AlertLayout = styled.div`
+    padding: 20px;
+`;
+
 const CustomerServiceContainerLayout = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  width: 100%;
-  height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    height: 100%;
 `;
 
 const ServiceLayout = styled.div`
-  margin-right: 16px;
+    margin-right: 16px;
 
-  @media (max-width: 1633px) {
-    &:nth-child(2n) {
-      margin-right: 0px;
+    @media (max-width: 1633px) {
+        &:nth-child(2n) {
+            margin-right: 0px;
+        }
+
+        &:nth-child(-1n + 2) {
+            margin-bottom: 16px;
+        }
     }
 
-    &:nth-child(-1n + 2) {
-      margin-bottom: 16px;
+    @media (max-width: 1024px) {
+        margin: 0px;
     }
-  }
-
-  @media (max-width: 1024px) {
-    margin: 0px;
-  }
 `;
 
 const Layouts = styled.div`
-  display: flex;
-  align-items: center;
+    display: flex;
+    align-items: center;
 
-  @media (max-width: 1633px) {
-    width: 556px;
-    flex-wrap: wrap;
-  }
+    @media (max-width: 1633px) {
+        width: 556px;
+        flex-wrap: wrap;
+    }
 
-  @media (max-width: 1023px) {
-    display: block;
-    width: 100%;
-    padding: 33px 16px;
-    height: calc(100vh - 64px);
-  }
+    @media (max-width: 1023px) {
+        display: block;
+        width: 100%;
+        padding: 33px 16px;
+        height: calc(100vh - 64px);
+    }
 `;
 
-const CustomerServiceContainer: React.FC<Iprops> = ({
-  questionType,
-  clickContact,
-  closeForm,
-}) => {
-  const dispatch = useDispatch();
-  const router = useRouter();
-  const { isSubmitSuccess } = useGetStore.customerService();
-  const { width } = useResize();
+const CustomerServiceContainer: React.FC<Iprops> = ({ questionType, clickContact, closeForm }) => {
+    const dispatch = useDispatch();
+    const router = useRouter();
+    const { isSubmitSuccess } = useGetStore.customerService();
+    const { width } = useResize();
+    const [isOpenAlert, setIsOpenAlert] = useState(true);
 
-  useEffect(() => {
-    if (Boolean(isSubmitSuccess)) {
-      router.push('/customer-service/inquiry');
-      dispatch(customerServiceActions.setIsSubmitSuccess(''));
-    }
-  }, [isSubmitSuccess]);
+    useEffect(() => {
+        if (Boolean(isSubmitSuccess)) {
+            router.push("/customer-service/inquiry");
+            dispatch(customerServiceActions.setIsSubmitSuccess(""));
+        }
+    }, [isSubmitSuccess]);
 
-  const clickSubmit = (form: IForm) => {
-    dispatch(customerServiceActions.postInquiryProduct(form));
-  };
+    const clickSubmit = (form: IForm) => {
+        dispatch(customerServiceActions.postInquiryProduct(form));
+    };
 
-  return (
-    <CustomerServiceContainerLayout>
-      <Layouts>
-        <ServiceLayout>
-          <CustomerServiceComponents.Service
-            title="견적 문의"
-            infoMessage="제품의 설치의 초기 견적 문의"
-            backgroundImageSrc={Images.EsitmateBackground}
-            clickContact={() => clickContact('estimate')}
-          />
-        </ServiceLayout>
-        <ServiceLayout>
-          <CustomerServiceComponents.Service
-            title="A/S 문의"
-            infoMessage="설치 후 고장 및 수리 관련 문의"
-            backgroundImageSrc={Images.ASBackground}
-            clickContact={() => clickContact('A/S')}
-          />
-        </ServiceLayout>
-        <ServiceLayout>
-          <CustomerServiceComponents.Service
-            title="그 외 문의"
-            infoMessage="기타 사항 문의"
-            backgroundImageSrc={Images.ETCBackground}
-            clickContact={() => clickContact('ETC')}
-          />
-        </ServiceLayout>
-        {width > 1023 && (
-          <CustomerServiceComponents.InfoCard
-            backgroundImageSrc={Images.InfoBackground}
-          />
-        )}
-      </Layouts>
-      {questionType && (
-        <CustomerServiceComponents.FormModal
-          questionType={questionType}
-          closeForm={closeForm}
-          clickSubmit={clickSubmit}
-        />
-      )}
-    </CustomerServiceContainerLayout>
-  );
+    return (
+        <CustomerServiceContainerLayout>
+            {isOpenAlert && (
+                <Widgets.Layout.ModalLayout
+                    onClose={() => {
+                        setIsOpenAlert(false);
+                    }}>
+                    <AlertLayout>
+                        현재 웹 사이트 리뉴얼 중입니다.
+                        <br /> 자세한 문의는 032-579-8272 번호로 연락주세요.
+                    </AlertLayout>
+                </Widgets.Layout.ModalLayout>
+            )}
+            <Layouts>
+                <ServiceLayout>
+                    <CustomerServiceComponents.Service
+                        title="견적 문의"
+                        infoMessage="제품의 설치의 초기 견적 문의"
+                        backgroundImageSrc={Images.EsitmateBackground}
+                        clickContact={() => clickContact("estimate")}
+                    />
+                </ServiceLayout>
+                <ServiceLayout>
+                    <CustomerServiceComponents.Service
+                        title="A/S 문의"
+                        infoMessage="설치 후 고장 및 수리 관련 문의"
+                        backgroundImageSrc={Images.ASBackground}
+                        clickContact={() => clickContact("A/S")}
+                    />
+                </ServiceLayout>
+                <ServiceLayout>
+                    <CustomerServiceComponents.Service
+                        title="그 외 문의"
+                        infoMessage="기타 사항 문의"
+                        backgroundImageSrc={Images.ETCBackground}
+                        clickContact={() => clickContact("ETC")}
+                    />
+                </ServiceLayout>
+                {width > 1023 && <CustomerServiceComponents.InfoCard backgroundImageSrc={Images.InfoBackground} />}
+            </Layouts>
+            {questionType && (
+                <CustomerServiceComponents.FormModal
+                    questionType={questionType}
+                    closeForm={closeForm}
+                    clickSubmit={clickSubmit}
+                />
+            )}
+        </CustomerServiceContainerLayout>
+    );
 };
 
 export default CustomerServiceContainer;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,125 +1,117 @@
-import React from 'react';
-import Head from 'next/head';
-import styled from 'styled-components';
-import { useRouter } from 'next/router';
-import { MainNav, Footer } from '@/components';
-import { AppProps } from 'next/app';
-import withReduxSaga from 'next-redux-saga';
-import { wrapper } from '@/store/configureStore';
-import { mixins } from '@/styles';
-import useResize from '@/hooks/useResize';
-import CssBaseline from '@mui/material/CssBaseline';
-import { ProductType } from '@/interfaces';
+import React, { useEffect } from "react";
+import Head from "next/head";
+import styled from "styled-components";
+import { useRouter } from "next/router";
+import { MainNav, Footer } from "@/components";
+import { AppProps } from "next/app";
+import withReduxSaga from "next-redux-saga";
+import { wrapper } from "@/store/configureStore";
+import { mixins } from "@/styles";
+import useResize from "@/hooks/useResize";
+import CssBaseline from "@mui/material/CssBaseline";
+import { ProductType } from "@/interfaces";
 
 interface IContainerProps {
-  height: number;
+    height: number;
 }
 
 declare global {
-  interface Window {
-    kakao: any;
-  }
+    interface Window {
+        kakao: any;
+    }
 }
 
 const STDContainer = styled.div<IContainerProps>`
-  ${mixins.flexSet('flex-start', 'flex-start')}
-  height: ${(props) => props.height}px;
-  background-color: #f0f0f0;
+    ${mixins.flexSet("flex-start", "flex-start")}
+    height: ${(props) => props.height}px;
+    background-color: #f0f0f0;
 
-  @media (max-width: 1024px) {
-    ${mixins.flexSet('flex-start', 'flex-start', 'column')}
-    height: auto;
-  }
+    @media (max-width: 1024px) {
+        ${mixins.flexSet("flex-start", "flex-start", "column")}
+        height: auto;
+    }
 `;
 
 function App({ Component, pageProps }: AppProps) {
-  const router = useRouter();
-  const selectedMenu = router.pathname;
-  const params = router.query as {
-    menu:
-      | 'welcome'
-      | 'history'
-      | 'way-to-come'
-      | 'performance'
-      | 'certification';
-  };
-  const { height } = useResize();
+    const router = useRouter();
+    const selectedMenu = router.pathname;
+    const params = router.query as {
+        menu: "welcome" | "history" | "way-to-come" | "performance" | "certification";
+    };
+    const { height } = useResize();
 
-  const onClickMenu = (menu: string) => {
-    router.push(`/${menu}`);
-  };
+    useEffect(() => {}, []);
 
-  const onClickProduct = (productType: ProductType['type']) => {
-    router.push(`/product/${productType}`);
-  };
+    const onClickMenu = (menu: string) => {
+        router.push(`/${menu}`);
+    };
 
-  const onClickCompanyMenu = (
-    companyMenu:
-      | 'welcome'
-      | 'history'
-      | 'way-to-come'
-      | 'performance'
-      | 'certification'
-  ) => {
-    if (companyMenu === 'certification') {
-      router.push(`/company/${companyMenu}?type=license`);
-    } else {
-      router.push(`/company/${companyMenu}`);
-    }
-  };
+    const onClickProduct = (productType: ProductType["type"]) => {
+        router.push(`/product/${productType}`);
+    };
 
-  const onClickNoticeMenu = (noticeMenu: 'list' | 'data') => {
-    router.push(`/notice/${noticeMenu}`);
-  };
+    const onClickCompanyMenu = (
+        companyMenu: "welcome" | "history" | "way-to-come" | "performance" | "certification"
+    ) => {
+        if (companyMenu === "certification") {
+            router.push(`/company/${companyMenu}?type=license`);
+        } else {
+            router.push(`/company/${companyMenu}`);
+        }
+    };
 
-  const onClickCustomerMenu = (customerMenu: 'inquiry') => {
-    router.push(`/customer-service/${customerMenu}`);
-  };
+    const onClickNoticeMenu = (noticeMenu: "list" | "data") => {
+        router.push(`/notice/${noticeMenu}`);
+    };
 
-  return (
-    <>
-      <Head>
-        <meta
-          content="정직과 신뢰, 폭 넓은 전문지식으로 고객 만족에 한 발 더 나아가겠습니다."
-          name="description"
-          key="description"
-        />
-        <meta
-          content="정직과 신뢰, 폭 넓은 전문지식으로 고객 만족에 한 발 더 나아가겠습니다."
-          property="og:description"
-          key="og:description"
-        />
-        <meta
-          content="정직과 신뢰, 폭 넓은 전문지식으로 고객 만족에 한 발 더 나아가겠습니다."
-          name="twitter:description"
-          key="twitter:description"
-        />
-        <meta
-          content="공기조화기, 동파방지댐퍼코일, 동파방지, 댐퍼코일, 배기유니트, 조화기, 버블댐퍼, 밀폐도어, 밀폐도어, 송풍기, 동주산업, 동주"
-          name="keywords"
-        />
-        <title>동주산업</title>
-        <meta content="동주산업" property="og:title" key="og:title" />
-        <meta content="동주산업" name="twitter:title" key="twitter:title" />
-        <meta content="동주산업" name="author" key="author" />
-        <meta content="동주산업" property="og:site_name" key="og:site_name" />
-      </Head>
-      <CssBaseline />
-      <STDContainer height={height}>
-        <MainNav
-          selectedMenu={selectedMenu}
-          params={params}
-          onClickMenu={onClickMenu}
-          onClickProduct={onClickProduct}
-          onClickNoticeMenu={onClickNoticeMenu}
-          onClickCompanyMenu={onClickCompanyMenu}
-          onClickCustomerMenu={onClickCustomerMenu}
-        />
-        <Component {...pageProps} />
-      </STDContainer>
-      {selectedMenu === '/' && <Footer />}
-    </>
-  );
+    const onClickCustomerMenu = (customerMenu: "inquiry") => {
+        router.push(`/customer-service/${customerMenu}`);
+    };
+
+    return (
+        <>
+            <Head>
+                <meta
+                    content="정직과 신뢰, 폭 넓은 전문지식으로 고객 만족에 한 발 더 나아가겠습니다."
+                    name="description"
+                    key="description"
+                />
+                <meta
+                    content="정직과 신뢰, 폭 넓은 전문지식으로 고객 만족에 한 발 더 나아가겠습니다."
+                    property="og:description"
+                    key="og:description"
+                />
+                <meta
+                    content="정직과 신뢰, 폭 넓은 전문지식으로 고객 만족에 한 발 더 나아가겠습니다."
+                    name="twitter:description"
+                    key="twitter:description"
+                />
+                <meta
+                    content="공기조화기, 동파방지댐퍼코일, 동파방지, 댐퍼코일, 배기유니트, 조화기, 버블댐퍼, 밀폐도어, 밀폐도어, 송풍기, 동주산업, 동주"
+                    name="keywords"
+                />
+                <title>동주산업</title>
+                <meta content="동주산업" property="og:title" key="og:title" />
+                <meta content="동주산업" name="twitter:title" key="twitter:title" />
+                <meta content="동주산업" name="author" key="author" />
+                <meta content="동주산업" property="og:site_name" key="og:site_name" />
+            </Head>
+            <CssBaseline />
+            <STDContainer height={height}>
+                <MainNav
+                    selectedMenu={selectedMenu}
+                    params={params}
+                    onClickMenu={onClickMenu}
+                    onClickProduct={onClickProduct}
+                    onClickNoticeMenu={onClickNoticeMenu}
+                    onClickCompanyMenu={onClickCompanyMenu}
+                    onClickCustomerMenu={onClickCustomerMenu}
+                />
+                <Component {...pageProps} />
+            </STDContainer>
+            {selectedMenu === "/" && <Footer />}
+        </>
+    );
 }
 
 export default wrapper.withRedux(withReduxSaga(App));
